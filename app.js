3b51124faefa3b0faa1e418692fb0b44c54b9c97
kendrajmoore <kendra.j.moore3443@gmail.com>
2021-06-06 16:51:10 -0400

    adding client side

--- app.js
+++ app.js
@@ -6,16 +6,47 @@ const dotenv = require("dotenv").config();
 app.use(express.static("public"))
 
 app.get("/data", async(req, res) => {
-    const query = `{ viewer { login } }`
+    const query = `
+    {
+      search(query: "stars:>50000", type: REPOSITORY, first: 10) {
+        repositoryCount
+        edges {
+          node {
+            ... on Repository {
+              name
+              owner {
+                login
+              }
+              stargazers {
+                totalCount
+              }
+            }
+          }
+        }
+      }
+    }
+  `
     const url = "https://api.github.com/graphql"
 
     const options = {
         method: "post",
         headers: {
             "content-type": "application/json",
-             authorization
-        }
+            authorization: "bearer " + process.env.APIKEY,
+        },
+        body: JSON.stringify({ query: query }),
     }
+
+    let response 
+    try {
+        response = await fetch(url, options)
+    } catch (error) {
+        console.error(error)
+    }
+    const data = await response.json()
+    console.log(data)
+
+    res.json(data)
     
 })
 
